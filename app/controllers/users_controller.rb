5edb34cb7806476cc71d7b829579f68a3c9ddb20
164matsubara <goatmailoffice@gmail.com>
2016-01-17 09:24:41 +0000

    asignment1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
   
   def show #追加
     @user = User.find(params[:id])
@@ -18,8 +20,24 @@ class UsersController < ApplicationController
     end
   end
   
+  def update
+    if @user.update(user_params)
+      redirect_to @user , notice: 'プロフィールを更新しました'
+    else render 'edit'
+    end
+  end
+  
+  def edit
+  end
+  
+  
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :age, :profile, :area)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
